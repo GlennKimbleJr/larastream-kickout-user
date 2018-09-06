6c3073418023238063eb56acd77eb1bb599e6aa8
GlennKimbleJr <glennkimble@yahoo.com>
2018-09-05 22:32:34 -0400

    Cleaned up the layout/template a bit.

--- resources/views/active.blade.php
+++ resources/views/active.blade.php
@@ -1,13 +1,38 @@
-<table>
-    @foreach ($users as $user)
-        <tr>
-            <td>{{ $user->name }}</td>
-            <td>
-                <form action="{{ route('admin.users.kick', $user) }}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    <button class="btn btn-sm btn-danger">Kick Out</button>
-                </form>
-            </td>
-    @endforeach
-</table>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Active Users</div>
+
+                <div class="card-body">
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <th>User</th>
+                            <th>Last Active</th>
+                            <th>Action(s)</th>
+                        </thead>
+                        <tbody>
+                        @foreach ($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->last_active_at }}</td>
+                                <td>
+                                    <form action="{{ route('admin.users.kick', $user) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                        <button class="btn btn-sm btn-danger">Kick Out</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,11 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        @auth
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.users.active') }}">Active Users</a>
+                        </li>
+                        @endguest
                     </ul>
 
                     <!-- Right Side Of Navbar -->
